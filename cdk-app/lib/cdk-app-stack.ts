ad0df638c08b195259a5b04041e2889e3febca8b
Pleng Nakdee <plengwn@gmail.com>
2023-04-11 11:42:51 +0700

    add stack

---
+++ cdk-app/lib/cdk-app-stack.ts
@@ -0,0 +1,25 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
+import { StateMachine, Succeed } from 'aws-cdk-lib/aws-stepfunctions';
+import { LambdaInvoke } from 'aws-cdk-lib/aws-stepfunctions-tasks';
+import { join, resolve } from 'path';
+
+export class CdkAppStack extends Stack {
+  private readonly lambdasDir = resolve(__dirname, 'lambdas');
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const calculateFunction = new Function(this, 'CalculateFunction', {
+      code: Code.fromAsset(join(this.lambdasDir, 'calculate.py')),
+      runtime: Runtime.PYTHON_3_9,
+      handler: "index.handler"
+    });
+
+    const stateMachine = new StateMachine(this, 'CalculationMachine', {
+      definition: new LambdaInvoke(this, "calculationTask", {
+        lambdaFunction: calculateFunction
+      }).next(new Succeed(this, "Success"))
+    });
+  }
+}
